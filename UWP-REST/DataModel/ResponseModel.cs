19cbf3edf7f0c0c3f38d30fa630027351c3a33b0
bjornops <bjornops@gmail.com>
2017-05-26 09:12:41 +0200

    MVVM attempt begun

---
+++ UWP-REST/DataModel/ResponseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_REST.DataModel
+{
+    class ResponseModel
+    {
+        public string Url { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public string ResponseHeaders { get; private set; }
+        public string ResponseBody { get; private set; }
+        public int ResponseTime { get; private set; }
+
+        public ResponseModel(string url, string responseHeader, string responseBody)
+        {
+            Url = url;
+            ResponseHeaders = responseHeader;
+            ResponseBody = responseBody;
+            Timestamp = DateTime.Now;
+        }
+
+    }
+}
---
+++ UWP-REST/PageModel/MainPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_REST.PageModel
+{
+    class MainPageModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
